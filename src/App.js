85c1ae62a2dbf1c5b931aa636ed8de47a7e2c0ef
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-04-20 20:08:47 -0700

    ebgan building app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+
+function App(props) {
+  const gifs = data;
+  const loading = false;
+  const error ="";
+
+
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Search for Gifs</h1>
+
+      <form>
+        <input />
+        <button>Search</button>
+      </form>
+
+      {
+        loading ? <h3>We are loading</h3> : <GifList gifs={gifs}/>
+      }
     </div>
   );
 }
